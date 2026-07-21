0f1650c8c2265b8c1a3f50db879d043cc10e8165
Yash Rajput <yash92726@gmail.com>
2026-07-21 07:28:05 +0530

    Add files via upload

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -2,15 +2,15 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
+// 👇 Yaha apna Firebase project config paste karo
+// Firebase Console → Project Settings → General → Your apps → Web app
 const firebaseConfig = {
-  apiKey: "AIzaSyDqLThvBn19ajSG1uqeA6JtnouA5u1RxuM",
-  authDomain: "yash-software.firebaseapp.com",
-  databaseURL: "https://yash-software-default-rtdb.firebaseio.com",
-  projectId: "yash-software",
-  storageBucket: "yash-software.firebasestorage.app",
-  messagingSenderId: "333775666671",
-  appId: "1:333775666671:web:4f9cbca22a8200b3d78a1a",
-  measurementId: "G-F747YYSCWX",
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID",
 };
 
 const app = initializeApp(firebaseConfig);
